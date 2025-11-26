5de6a1c7e185dff6726c2e67d31ab74f5b3c8bf2
Hung232006 <24107264@st.phenikaa-uni.edu.vn>
2025-11-26 17:34:31 +0700

    cart.html

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
+from app.models import db, CartItem, Product
 
 main_bp = Blueprint('main', __name__)
 
@@ -7,3 +8,15 @@ main_bp = Blueprint('main', __name__)
 @login_required
 def index():
     return render_template('_index.html')
+
+@main_bp.route('/cart')
+@login_required
+def view_cart():
+    user_id = current_user.id
+
+    cart_items = db.session.query(CartItem, Product)\
+        .join(Product, CartItem.product_id == Product.id)\
+        .filter(CartItem.user_id == user_id)\
+        .all()
+
+    return render_template('cart.html', cart_items=cart_items)
